133ba1cc865e7cd70617cb2f9e9aa49643a83644
Julien Cassis <juliencassis@gmail.com>
2021-09-10 12:31:48 -0400

    bumped sparkle: snapshot aggregation optimization

--- devel/exchange-pl.sh
+++ devel/exchange-pl.sh
@@ -2,6 +2,11 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 STOREURL=gs://dfuseio-global-blocks-us/eth-bsc-mainnet/v1
+if [ "$RPCENDPOINT" == "" ]; then
+	echo "you need to set RPCENDPOINT var to your 'https://.../privateendpoint/' URL"
+	exit 1
+fi
+RPCCACHE=./localrpccache
 if test -d ./localblocks; then
   echo "Using blocks from local store: ./localblocks"
     STOREURL=./localblocks
@@ -14,25 +19,26 @@ mkdir ./localblocks
 gsutil -m cp "gs://dfuseio-global-blocks-us/eth-bsc-mainnet/v1/0006809*" ./localblocks/
 gsutil -m cp "gs://dfuseio-global-blocks-us/eth-bsc-mainnet/v1/0006810*" ./localblocks/
 gsutil -m cp "gs://dfuseio-global-blocks-us/eth-bsc-mainnet/v1/0006811*" ./localblocks/
+gsutil -m cp "gs://dfuseio-global-blocks-us/eth-bsc-mainnet/v1/0007000**" ./localblocks/
 
 ######
 EOC
 fi
 
 function step1() {
-    DEBUG=.* exchange parallel step -s 1 --output-path ./step1-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL&
+    DEBUG=.* exchange parallel step -s 1 --enable-aggregate-snapshot-save --output-path ./step1-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL --rpc-cache-load-path $RPCCACHE --rpc-cache-save-path $RPCCACHE --rpc-endpoint $RPCENDPOINT &
 }
 function step2() {
-    DEBUG=.* exchange parallel step -s 2 --input-path ./step1-v1 --output-path ./step2-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL&
+    DEBUG=.* exchange parallel step -s 2 --enable-aggregate-snapshot-save --input-path ./step1-v1 --output-path ./step2-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL --rpc-cache-load-path $RPCCACHE --rpc-cache-save-path $RPCCACHE --rpc-endpoint $RPCENDPOINT &
 }
 function step3() {
-    DEBUG=.* exchange parallel step -s 3 --input-path ./step2-v1 --output-path ./step3-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL&
+    DEBUG=.* exchange parallel step -s 3 --enable-aggregate-snapshot-save --input-path ./step2-v1 --output-path ./step3-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL --rpc-cache-load-path $RPCCACHE --rpc-cache-save-path $RPCCACHE --rpc-endpoint $RPCENDPOINT &
 }
 function step4() {
-    DEBUG=.* exchange parallel step -s 4 --input-path ./step3-v1 --output-path ./step4-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL&
+    DEBUG=.* exchange parallel step -s 4 --enable-aggregate-snapshot-save --input-path ./step3-v1 --output-path ./step4-v1 --start-block $1 --stop-block $2 --blocks-store-url $STOREURL --rpc-cache-load-path $RPCCACHE --rpc-cache-save-path $RPCCACHE --rpc-endpoint $RPCENDPOINT &
 }
 function step5() {
-    DEBUG=.* exchange parallel step -s 5 --flush-entities --store-snapshot=false --input-path ./step4-v1 --output-path ./step5-v1  --start-block $1 --stop-block $2  --blocks-store-url $STOREURL  --enable-poi &
+    DEBUG=.* exchange parallel step -s 5 --enable-aggregate-snapshot-save --flush-entities --store-snapshot=false --input-path ./step4-v1 --output-path ./step5-v1  --start-block $1 --stop-block $2  --blocks-store-url $STOREURL  --enable-poi --rpc-cache-load-path $RPCCACHE --rpc-cache-save-path $RPCCACHE --rpc-endpoint $RPCENDPOINT &
 }
 
 
@@ -49,6 +55,9 @@ main() {
       step1 7000000 7000009
       step1 7000010 7000019
       step1 7000020 7000029
+      step1 7000030 7000039
+      step1 7000040 7000049
+      step1 7000050 7000059
 
       for job in `jobs -p`; do
           echo "Waiting on $job"
@@ -65,6 +74,9 @@ main() {
       step2 7000000 7000009
       step2 7000010 7000019
       step2 7000020 7000029
+      step2 7000030 7000039
+      step2 7000040 7000049
+      step2 7000050 7000059
 
       for job in `jobs -p`; do
           echo "Waiting on $job"
@@ -78,9 +90,12 @@ main() {
       echo "LAUNCHING STEP 3"
       rm -rf ./step3-v1
 
-      step3 7000000 7000009
-      step3 7000010 7000019
-      step3 7000020 7000029
+#      step3 7000000 7000009
+#      step3 7000010 7000019
+#      step3 7000020 7000029
+#      step3 7000030 7000039
+      step3 7000040 7000049
+#      step3 7000050 7000059
 
       for job in `jobs -p`; do
           echo "Waiting on $job"
@@ -97,6 +112,9 @@ main() {
       step4 7000000 7000009
       step4 7000010 7000019
       step4 7000020 7000029
+      step4 7000030 7000039
+      step4 7000040 7000049
+      step4 7000050 7000059
 
       for job in `jobs -p`; do
           echo "Waiting on $job"
@@ -113,6 +131,9 @@ main() {
       step5 7000000 7000009
       step5 7000010 7000019
       step5 7000020 7000029
+      step5 7000030 7000039
+      step5 7000040 7000049
+      step5 7000050 7000059
 
       for job in `jobs -p`; do
           echo "Waiting on $job"
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/streamingfast/eth-go v0.0.0-20210831180555-8d52c827993b
 	github.com/streamingfast/logging v0.0.0-20210811175431-f3b44b61606a
-	github.com/streamingfast/sparkle v0.0.0-20210910155848-ce125463a43f
+	github.com/streamingfast/sparkle v0.0.0-20210910163029-8dfb95f44634
 	github.com/stretchr/testify v1.7.1-0.20210427113832-6241f9ab9942
 	github.com/test-go/testify v1.1.4
 	github.com/ugorji/go v1.2.6 // indirect
